94df99f7eb38828f884e730bd8cd01e9525df36c
Shawn Eary <shawn.eary@live.com>
2022-02-14 14:47:51 -0600

    Added Story Time controller.

---
+++ utilars-core/Controllers/StoryTimeController.fs
@@ -0,0 +1,20 @@
+﻿namespace utilars_core.Controllers
+
+open System.Diagnostics
+
+open Microsoft.AspNetCore.Mvc
+// ©2022 - Shawn Eary
+//
+// Licensed via FCML:
+// https://www.utilars.com/Home/FCML
+open Microsoft.Extensions.Logging
+
+open utilars_core.Models
+
+type StoryTimeController (logger : ILogger<StoryTimeController>) =
+    inherit Controller()
+
+    member this.Index () =
+        this.View()
+    member this.Credits () =
+        this.View()
