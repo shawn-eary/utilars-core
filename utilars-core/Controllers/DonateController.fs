1fd93ecdca01b20ef1a21ef7ae44fb5ba2bda62f
shawn.eary <shawn.eary@live.com>
2023-01-07 11:11:17 -0600

    Added a PayPal Donate button.

---
+++ utilars-core/Controllers/DonateController.fs
@@ -0,0 +1,24 @@
+﻿namespace utilars_core.Controllers
+
+open System.Collections.Generic
+open System.Diagnostics
+
+open Microsoft.AspNetCore.Mvc
+// ©2013, 2021-2023 - Shawn Eary
+//
+// Licensed via FCML:
+// https://www.utilars.com/Home/FCML
+open Microsoft.Extensions.Logging
+
+open utilars_core.Models
+
+open System.Net.Mail
+open System.Net
+
+type DonateController (logger : ILogger<SupportController>) =
+    inherit Controller()
+
+    [<HttpGet>]
+    member this.Index () =
+        this.ViewData.["Message"] <- "Thank you"
+        this.View() :> ActionResult
