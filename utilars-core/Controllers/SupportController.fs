8fe8051b8f85e0a36f23b3f694c4b48e908a4539
shawn.eary <shawn.eary@live.com>
2023-01-06 21:09:39 -0600

    Added basic support ticket page.

---
+++ utilars-core/Controllers/SupportController.fs
@@ -0,0 +1,95 @@
+﻿namespace utilars_core.Controllers
+
+open System.Collections.Generic
+open System.Diagnostics
+
+open Microsoft.AspNetCore.Mvc
+// ©2013, 2021-2023 - Shawn Eary
+//
+// Licensed via FCML:
+// https://www.utilars.com/Home/FCML
+open Microsoft.Extensions.Logging
+
+open utilars_core.Models
+
+open System.Net.Mail
+open System.Net
+
+type SupportController (logger : ILogger<SupportController>) =
+    inherit Controller()
+
+    // member this.Index () =
+    //     x.
+    //     this.ViewData.["Message"] <- "Welcome to Utilars™"
+    //     this.View()
+    [<HttpGet>]
+    member this.Index () =
+        this.ViewData.["Message"] <- "Utilars™ Support Request"
+
+        // https://www.c-sharpcorner.com/article/redirect-action-result-in-asp-net-core-mvc/
+        this.RedirectToAction("Ticket","Support")
+    member x.Ticket (product : string) = 
+        let theModel = new utilars_core.Models.TicketRequest()
+        x.View(theModel) :> ActionResult
+    // References: 
+    // http://msdn.microsoft.com/en-us/library/system.net.mail.smtpclient.credentials.aspx
+    [<HttpPost>]
+    member x.Ticket (someTicketRequest : utilars_core.Models.TicketRequest) : ActionResult =        
+        if x.ModelState.IsValid then
+           let response = someTicketRequest.ChallengeResponse
+           let expected = x.Request.Form.Item("Expected")
+           let result = 
+               logicLibrary.imgGenerator.responseIsValid response expected
+           if result then
+               let thePassword = "[Sanitized]"
+               let forwardingAccount:string = "[Sanitized]"
+               let theServer:string = "[Sanitized]"
+               let thePort:int = -1
+
+
+               let userEmail = someTicketRequest.Email
+               let userMsg = someTicketRequest.Message
+               let theProduct: string = "OATS 1.0"
+
+               let combinedMsg = 
+                  "Utilars™ Support Request\n\r" +
+                  "\n\r" + 
+                  "Customer Email   : " + userEmail + "\n\r" + 
+                  "Customer Name    : " + someTicketRequest.Name + "\n\r" + 
+                  "Customer Product : " + theProduct + "\n\r" + 
+                  "Message : \n\r" + userMsg
+               let myEmailClient = new SmtpClient(theServer, thePort)
+               myEmailClient.EnableSsl <- true            
+               myEmailClient.UseDefaultCredentials <- false
+
+               let myCredentials = 
+                   new NetworkCredential(
+                      forwardingAccount, 
+                      thePassword, 
+                      ""
+                    )
+               myEmailClient.Credentials <- myCredentials
+               myEmailClient.Send(
+                   forwardingAccount,
+                   "[Sanitized]," + userEmail,
+                   "msg From:" + userEmail, 
+                   combinedMsg
+                )
+
+               x.ViewData.["Message"] <- 
+                   "Your request has been forwarded to support"
+               x.View("MessageToUser") :> ActionResult
+            else
+                x.ViewData.["MessageToUser"] <- 
+                    "It appears that you didn't enter the correct " +
+                    "response for the number challenge."
+                let expectedAuth = logicLibrary.imgGenerator.getImages(0)
+                someTicketRequest.Base64Digits <- fst(expectedAuth)
+                someTicketRequest.Expected <- snd(expectedAuth)
+                x.View("Ticket", someTicketRequest) :> ActionResult
+         else
+             (* User entered bogus data.  Let him/her try again *)
+             let expectedAuth = logicLibrary.imgGenerator.getImages(0)                   
+             someTicketRequest.Base64Digits <- fst(expectedAuth)
+             someTicketRequest.Expected <- snd(expectedAuth)
+             x.View("Ticket", someTicketRequest) :> ActionResult
---
+++ utilars-core/Models/SupportModels.fs
@@ -0,0 +1,55 @@
+﻿// ©2013 - Shawn Eary
+namespace utilars_core.Models
+
+open System.ComponentModel.DataAnnotations
+open System.ComponentModel
+open logicLibrary
+
+type TicketRequest() = 
+   let expectedAuth = logicLibrary.imgGenerator.getImages(0)          
+   let mutable base64Digits = fst(expectedAuth)
+   let mutable expected = snd(expectedAuth)
+   let mutable challengeResponse = ""
+   let mutable name = ""
+   let mutable email = ""
+   let mutable message = ""
+       
+   [<DataType(DataType.Text)>]
+   [<DisplayName("base64Digits")>]
+   member x.Base64Digits with get() = base64Digits and set(value) = base64Digits <- value
+
+   [<Required>]
+   [<DataType(DataType.Text)>]
+   [<DisplayName("expected")>]
+   member x.Expected with get() = expected and set(value) = expected <- value
+   
+   [<Required(ErrorMessage = "Must enter numbers above")>]
+   [<StringLength(10)>]
+   [<DataType(DataType.Text)>]      
+   [<RegularExpression("^[0-9]+$", ErrorMessage = "Only Digits Allowed.")>]
+   member x.ChallengeResponse 
+      with get() = challengeResponse and set(value) = challengeResponse <- value
+
+   [<Required(ErrorMessage = "Please give your name")>]
+   [<StringLength(50)>]
+   [<DataType(DataType.Text)>]
+   [<RegularExpression("^[a-zA-Z ]+$", ErrorMessage = "Please only use standard English letters and spaces.")>]
+   member x.Name
+      with get() = name and set(value) = name <- value
+
+   [<Required(ErrorMessage = "Please provide your email address")>]
+   [<StringLength(50)>]
+   [<DataType(DataType.EmailAddress)>]
+   // https://stackoverflow.com/questions/16712043/email-address-validation-using-asp-net-mvc-data-type-attributes
+   [<EmailAddress(ErrorMessage = "Invalid Email Address")>]
+   member x.Email
+      with get() = email and set(value) = email <- value
+
+   [<Required(ErrorMessage = "Please describe the anomally or need")>]
+   [<StringLength(500)>]
+   [<DataType(DataType.Text)>]
+   // https://stackoverflow.com/questions/16712043/email-address-validation-using-asp-net-mvc-data-type-attributes
+   [<RegularExpression("^[a-zA-Z,! .]+$", ErrorMessage = "Please only use letters, spaces, periods and commas.")>]
+   member x.Message
+      with get() = message and set(value) = message <- value
+
